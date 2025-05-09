35b3f995a2e9423f85847f0e19929c2b3ac3848a
rohitxess <xessro@gmail.com>
2025-05-09 17:40:40 +0530

    added code for components, hero and layout

--- coffee/src/App.jsx
+++ coffee/src/App.jsx
@@ -1,16 +1,30 @@
 import Layout from "./components/Layout";
 import Hero from "./components/Hero";
 import CoffeeForm from "./components/CoffeeForm";
+import Stats from "./components/Stats"
+import History from "./components/History"
 
 function App(){
   
   // it is a good practice to make a dummy data 
   const isAuthenticated = false;
+  
+  const authenticatedContent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
 
   return (
     <Layout>
       <Hero />
       <CoffeeForm />
+      {isAuthenticated && (authenticatedContent)} 
     </Layout>
   )
 }
+
+
+export default App
+// if the authentication is false do not show the 
--- coffee/src/components/Layout.jsx
+++ coffee/src/components/Layout.jsx
@@ -3,11 +3,23 @@ export default function Layout(props) {
     const { children } = props;
     
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFEINED</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up for free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p><span className="text-gradient">Caffeined</span>was made by <a href="" target="_blank">RohitX</a>using the 
+            <a target="_blank" href="">FantaCss</a>design library.</p>
+        </footer>
     )
     return (
         <>
