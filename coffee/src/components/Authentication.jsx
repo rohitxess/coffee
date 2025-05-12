61f834e144b6f5c3a209a81ceb748f5401e7b6d3
rohitxess <xessro@gmail.com>
2025-05-12 15:03:45 +0530

    bootstraped database component

--- coffee/src/components/Authentication.jsx
+++ coffee/src/components/Authentication.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 
 
 export default function Authentication () {
+    const [ isRegistration, setIsRegistration ] = useState(false);
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ isauthenticating, setIsAuthenticating ] = useState(false);
+
+    async function handleAuthenticate() {
+
+    }
+
     return (
         <>
-        <h2 className="sign-up-text">Sign up/ Login</h2>
-        <p>Sing in to your account</p>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="*******" />
-        <button>
+        <h2 className="sign-up-text">{ isRegistration ? 'Sign up' : 'Login'}</h2>
+        <p>{isRegistration ? 'Create an account' : 'Sign in to your account!'}</p>
+        <input value={email} type="text" placeholder="Email" 
+        onChange={(e) => {
+            setEmail(e.target.value)
+        }}/>
+        <input value={password} type="password" placeholder="*******" 
+         onChange={(e) => {
+            setPassword(e.target.value)
+        }}/>
+        <button onClick={handleAuthenticate}>
             <p>Submit</p>
         </button>
         <hr />
         <div className="register-content">
-            <p>Don&apos;t have an account</p>
-            <button>
-                <p>Sign up</p>
+            <p>{ isRegistration ? 'Already have an account' : 'Dont have an account'}</p>
+            <button onClick={() => {setIsRegistration(!isRegistration)} }>
+                <p>{ isRegistration ? 'Sign in' : 'Sign Up'}</p>
             </button>
         </div>
         </>
